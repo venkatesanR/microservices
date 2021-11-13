dd8ef72268015a02e687aeccd52c5c8162796fbc
Venkatesan Rengasamy <venkatraman1729@gmail.com>
2021-11-13 10:45:38 +0530

    NOJIRA: Updated config in Microservices

--- configuration-service/src/main/java/com/microservices/config/ConfigurationServiceApp.java
+++ configuration-service/src/main/java/com/microservices/config/ConfigurationServiceApp.java
@@ -2,9 +2,10 @@ package com.microservices.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableConfigServer
 public class ConfigurationServiceApp {
     public static void main(String[] args) {
